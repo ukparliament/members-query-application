0f40ed8691b167e25f5ee8f22141d6589cb4edeb
Rebecca Appleyard <rklappleyard@gmail.com>
2016-09-15 10:21:28 +0100

    Renaming from member to person in some tests

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'PeopleController', :type => :controller do
-  xdescribe "GET index" do
+  describe "GET index" do
     it 'can render data in json format' do
       get 'index', :format => :json
 
