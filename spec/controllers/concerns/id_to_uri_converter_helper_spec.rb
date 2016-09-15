798cff5b98e5ae2dfe399765e97a7c2fa254e15a
Rebecca Appleyard <rklappleyard@gmail.com>
2016-09-15 16:23:55 +0100

    Renamed convert_id_to_uri method

--- spec/controllers/concerns/id_to_uri_converter_helper_spec.rb
+++ spec/controllers/concerns/id_to_uri_converter_helper_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 describe IdToUriConverterHelper do
   let(:extended_class) { Class.new { extend IdToUriConverterHelper } }
 
-  describe '#convert_id_to_uri' do
+  describe '#convert_id_to_resource_uri' do
     it 'should take an id and convert it to a uri for the endpoint' do
-      expect(extended_class.convert_id_to_uri('1')).to eq 'http://id.ukpds.org/1'
+      expect(extended_class.convert_id_to_resource_uri('1')).to eq 'http://id.ukpds.org/1'
     end
   end
 end
