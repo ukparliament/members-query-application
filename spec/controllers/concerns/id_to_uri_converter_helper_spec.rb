39a2024a3e6da40e9e2f639abc9da5c53575d4f4
Rebecca Appleyard <rklappleyard@gmail.com>
2016-09-15 16:22:27 +0100

    Added test for convert_id_uri method

--- spec/controllers/concerns/id_to_uri_converter_helper_spec.rb
+++ spec/controllers/concerns/id_to_uri_converter_helper_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 describe IdToUriConverterHelper do
-  let(:included_class) { Class.new { include IdToUriConverterHelper } }
+  let(:extended_class) { Class.new { extend IdToUriConverterHelper } }
 
   describe '#convert_id_to_uri' do
-
+    it 'should take an id and convert it to a uri for the endpoint' do
+      expect(extended_class.convert_id_to_uri('1')).to eq 'http://id.ukpds.org/1'
+    end
   end
 end
