0c9b2a90e663d33be6ae81842a89e2783b1471b6
giusepped <giusdesan@gmail.com>
2016-09-16 09:59:26 +0100

    finished automated specs for #find

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -78,20 +78,64 @@ RSpec.describe 'PeopleController', :type => :controller do
       allow(PersonQueryObject).to receive(:find).with('http://id.ukpds.org/members/1').and_return({graph: PERSON_ONE_GRAPH, hierarchy: PERSON_ONE_HASH })
     end
 
-    it 'can render data in json format' do
-      get 'show', id: 'members/1', format: :json
+    context 'when the requested format is JSON' do
+      before(:each) do
+        get 'show', id: 'members/1', format: :json
+      end
+      it 'returns OK reponse with correct format' do
+        expect(response.status).to eq 200
+        expect(response.content_type).to eq 'application/json'
+      end
 
-      expect(response.status).to eq 200
-      expect(response.content_type).to eq 'application/json'
-      expect(json["people"][0]["id"]).to eq '1'
+      it 'returns only one person in the graph' do
+        expect(json['people'].length).to eq 1
+      end
+
+      it 'returns the correct id for the person' do
+        expect(json['people'][0]['id']).to eq '1'
+      end
     end
 
-    it 'can render data in xml format' do
-      get 'show', id: 'members/1', format: :xml
+    context 'when the requested format is XML' do
+      before(:each) do
+        get 'show', id: 'members/1', format: :xml
+      end
+      it 'returns OK reponse with correct format' do
+        expect(response.status).to eq 200
+        expect(response.content_type).to eq 'application/xml'
+      end
 
-      expect(response.status).to eq 200
-      expect(response.content_type).to eq 'application/xml'
-      expect(xml.xpath("//person")[0].children.children[0].content).to eq '1'
+      it 'returns only one person in the graph' do
+        expect(xml.xpath('//person').count).to eq 1
+      end
+
+      it 'returns the correct id for the person' do
+        expect(xml.xpath('//person')[0].children.children[0].content).to eq '1'
+      end
+    end
+
+    context 'when the requested format is RDF' do
+      before(:each) do
+        get 'show', id: 'members/1', format: :rdf
+      end
+      it 'returns OK reponse with correct format' do
+        expect(response.status).to eq 200
+        expect(response.content_type).to eq 'application/rdf+xml'
+      end
+
+      it 'returns only one person in the graph' do
+        expect(rdf.count).to eq 1
+      end
+
+      it 'returns the correct data for the person' do
+        expect(rdf.first).to eq PERSON_STATEMENTS[0]
+      end
+    end
+
+    context 'when the requested format is HTML' do
+      it 'raises an unknown format error' do
+        expect{ get 'show', id: 'members/1', format: :html }.to raise_error(ActionController::UnknownFormat)
+      end
     end
   end
 
